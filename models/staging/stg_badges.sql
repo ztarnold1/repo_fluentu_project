e57bf0fe09fbef71886b3d40b9c36a20f955b310
ztarnold1 <67172229+ztarnold1@users.noreply.github.com>
2020-06-24 16:45:00 -0500

    filtered badges and users (#6)

--- models/staging/stg_badges.sql
+++ models/staging/stg_badges.sql
@@ -1,3 +1,8 @@
+/*I will be using the badges table to determine "experts" that should be answering these unanswered, popular questions.
+I'm defining expert as a user who was awarded a Gold level badge (class = 1) in the last ~two years (beginning of 2018 to current) in the respective badge area. 
+I only care about badges that are tag based (tag based = true), because I will use the tag based badges as a filter for which questions the respective expert should answer.
+*/
+
 select 
 badge_id,
 name as badge_name,
@@ -6,3 +11,6 @@ user_id,
 class,
 tag_based
  from {{ source('stack_overflow_project', 'badges') }}
+ where tag_based = true
+ AND class = 1
+ AND date(date) >= DATE('2018-01-01')
