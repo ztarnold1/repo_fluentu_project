5027a9319987c6407189356d021a0ee293624bd2
Zac Arnold <ztarnold1@gmail.com>
2020-06-24 21:44:17 +0000

    filtered badges and users

--- models/staging/stg_users.sql
+++ models/staging/stg_users.sql
@@ -1,3 +1,6 @@
+/*I am filtering to only users who have accessed the site since 05/01/2020, which is the filter I've placed on the posts.
+*/
+
 SELECT 
 user_id,
 display_name,
@@ -10,3 +13,4 @@ reputation,
 up_votes, 
 views
 FROM {{ source('stack_overflow_project','users')}}
+where date(last_access_date) >= date('2020-05-01')
