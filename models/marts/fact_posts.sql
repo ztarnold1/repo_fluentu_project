e3ae3821fba5ddb98b6c3915d8138b73bda08402
ztarnold1 <67172229+ztarnold1@users.noreply.github.com>
2020-06-25 11:57:42 -0500

    modified post filters and added post tags table (#7)

--- models/marts/fact_posts.sql
+++ models/marts/fact_posts.sql
@@ -1,6 +1,7 @@
 /*For unanswered questions (answer count = 0), we only care about questions that are Open, and that are popular/trending. 
 Questions that aren't open are being filtered out by the lookup to the stg_post_history and stg_post_history_type_id_text table.
-Popular/trending is defined by me as having an view count >= 10 AND a score of >= 5.
+Popular/trending is defined by me as having an view count >= 10 AND a score of >= 1. I've changed the score from 5 to 1 because I felt
+5 was too restrictive, considering I saw several popular posts (~8k views) with only 2 upvotes.
 */
 
 with history_type as (
@@ -35,8 +36,7 @@ popular_unanswered_questions as (
     inner join open_posts using (post_id)
     where answer_count = 0 
     AND post_type_id = 1 --this means it was a question
-    AND view_count >= 20 
-    AND score >= 5 -- this is a popular question
+    AND (view_count >= 20 AND score >= 1) -- this is a popular question
 
     ),
 
--- models/staging/stg_posts_text.sql
+++ models/staging/stg_posts_text.sql
@@ -3,12 +3,14 @@ SELECT
 post_id, 
 title as post_title, 
 body as post_body, 
-tags
+tags,
+IFNULL((length(tags) - length(replace(tags,'|','')) + 1),0) as number_of_tags
  FROM {{ source('stack_overflow_project', 'posts_answers_text') }}
  UNION ALL
 SELECT 
 post_id, 
 title as post_title, 
 body as post_body, 
-tags
+tags,
+IFNULL((length(tags) - length(replace(tags,'|','')) + 1),0) as number_of_tags
  FROM {{ source('stack_overflow_project', 'posts_questions_text') }} 
